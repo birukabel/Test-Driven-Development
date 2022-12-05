aead7df9c924b60fed418306903be40cb93766bf
birukabel <biruk.abel@yahoo.com>
2022-12-04 20:18:54 -0800

    first commit

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_MadeByUser_ReturnsTrue()
+        {
+            var reservation = new Reservation();
+            
+            var result = reservation.CanBeCancelledBy(reservation.MadeBy);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
